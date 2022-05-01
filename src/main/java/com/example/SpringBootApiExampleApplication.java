54130591ab2dfc00c7772773085d819f52103a7c
Dhiren <dhirenp.singh@gmail.com>
2022-05-01 23:53:20 +0530

    First commit, Created the themepark Application

---
+++ src/main/java/com/example/SpringBootApiExampleApplication.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.entity.ThemeParkRide;
+import com.example.repository.ThemeParkRideRepository;
+
+@SpringBootApplication
+
+public class SpringBootApiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiExampleApplication.class, args);
+	}
+	
+	@Bean
+    public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+        return (args) -> {
+            repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+            repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+            repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+        };
+    }
+
+}
